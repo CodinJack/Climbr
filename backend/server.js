d26d052b30ec67fd28d1a017db8a6a8e4ac65b4a
CodinJack <IIT2022005@iiita.ac.in>
2024-06-17 12:38:36 +0300

    frontend

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,61 @@ app.use(express.json());
 const url = "mongodb+srv://jaikhanna615:xQ9EPYcArWbjx7aK@chatcluster.fxx8b1c.mongodb.net/?retryWrites=true&w=majority&appName=ChatCluster"
 
 
+const connect = mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    socket.on('Sent Message', (payload) => {
+      //store in db
+      connect.then((db) => {
+        try {
+          const chat = new Chat({
+            message: payload.message,
+            sender: payload.userId,
+          });
+          chat.save((err, data) => {
+            if (err) return res.json({ success: false, err });
+            Chat.find({ _id: data._id })
+              .populate('sender')
+              .exec((err, data) => {
+                return io.emit('Received Message', data);
+              });
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    });
+  });
+
+  const users = require('./routes/users');
+  const chat = require('./routes/chat');
+  
+  app.use('/users', users);
+  app.use('/chat', chat);
+  
+  // Serve static assets if in production
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    // index.html for all page routes
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  server.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+  
+  
