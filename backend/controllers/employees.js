eb997ad79a4db05cd811627b23e788111c356a59
CodinJack <IIT2022005@iiita.ac.in>
2024-06-18 00:34:02 +0300

    controller folder added

---
+++ backend/controllers/employees.js
@@ -0,0 +1,28 @@
+const Employee = require('../models/Employee');
+
+// Get all employees
+exports.getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get a single employee by ID
+exports.getEmployeeById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const employee = await Employee.findById(id);
+    if (!employee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+    res.json(employee);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//other employee controller functions - create, update, delete)
--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const Employee = require('../models/employeeModel');
-
 const router = express.Router();
+const emp = require('../controllers/employees'); 
 
+router.get('/', employeeController.getEmployees)
+      .get('/:id', employeeController.getEmployeeById)
+      
 
 module.exports = router;
