f2afd92371bd94656dd42dfc7943ab0ad85833cd
CodinJack <IIT2022005@iiita.ac.in>
2024-08-11 07:05:14 +0530

    login bug fix

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -7,22 +7,6 @@ router.get('/', taskController.getTasks)
       .post('/', taskController.createTask)
       .delete('/:id', taskController.deleteTask)
       .patch('/:id',taskController.patchTask)
-      .patch('/:id/verify', auth, isManager, async (req, res) => {
-            try {
-              const task = await Task.findById(req.params.taskId);
-              if (!task) return res.status(404).json({ message: 'Task not found' });
-          
-              task.completed = true;
-              await task.save();
-          
-              const employee = await Employee.findById(task.assignedTo);
-              employee.totalPoints += task.points;
-              await employee.save();
-          
-              res.json({ message: 'Task verified and employee points updated' });
-            } catch (error) {
-              res.status(500).json({ message: 'Server error' });
-            }
-          });
+      .patch('/:id/verify', taskController.verifyTask);
           
 module.exports = router;
